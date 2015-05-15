da5863ac58d676c8ac0c432493fe127593d2f0d3
Benj Sicam <benj@benjsicam.me>
2015-05-15 15:02:09 +0800

    Used clone instead of extend
    - Used clone instead of extend. Some complications with array output.

--- atrack/atrack.converters.js
+++ atrack/atrack.converters.js
@@ -1,40 +1,38 @@
 exports.convert = function () {
-    var convertedData = _.extend({}, data);
+	var convertedData = _.clone(data, true);
 
-    if (convertedData.gps_dtm)
-        convertedData.gps_dtm = moment(convertedData.gps_dtm, 'YYYYMMDDHHmmss').toDate();
+	if (convertedData.gps_dtm)
+		convertedData.gps_dtm = moment(convertedData.gps_dtm, 'YYYYMMDDHHmmss').toDate();
 
-    if (convertedData.dtm)
-        convertedData.dtm = moment(convertedData.dtm, 'YYYYMMDDHHmmss').toDate();
+	if (convertedData.dtm)
+		convertedData.dtm = moment(convertedData.dtm, 'YYYYMMDDHHmmss').toDate();
 
-    if (convertedData.position_dtm)
-        convertedData.position_dtm = moment(convertedData.position_dtm, 'YYYYMMDDHHmmss').toDate();
+	if (convertedData.position_dtm)
+		convertedData.position_dtm = moment(convertedData.position_dtm, 'YYYYMMDDHHmmss').toDate();
 
-    if (convertedData.coordinates[0])
-        convertedData.coordinates[0] = parseFloat(convertedData.coordinates[0]);
+	if (convertedData.coordinates[0])
+		convertedData.coordinates[0] = parseFloat(convertedData.coordinates[0]);
 
-    if (convertedData.coordinates[1])
-        convertedData.coordinates[1] = parseFloat(convertedData.coordinates[1]);
+	if (convertedData.coordinates[1])
+		convertedData.coordinates[1] = parseFloat(convertedData.coordinates[1]);
 
-    if (convertedData.heading)
-        convertedData.heading = parseInt(convertedData.heading);
+	if (convertedData.heading)
+		convertedData.heading = parseInt(convertedData.heading);
 
-    if (convertedData.event_code)
-        convertedData.event_code = parseInt(convertedData.event_code);
+	if (convertedData.event_code)
+		convertedData.event_code = parseInt(convertedData.event_code);
 
-    if (convertedData.odometer)
-        convertedData.odometer = parseInt(convertedData.odometer);
+	if (convertedData.odometer)
+		convertedData.odometer = parseInt(convertedData.odometer);
 
-    if (convertedData.gps_hdop)
-        convertedData.gps_hdop = parseInt(convertedData.gps_hdop);
+	if (convertedData.gps_hdop)
+		convertedData.gps_hdop = parseInt(convertedData.gps_hdop);
 
-    if (convertedData.speed)
-        convertedData.speed = parseInt(convertedData.speed);
+	if (convertedData.speed)
+		convertedData.speed = parseInt(convertedData.speed);
 
-    if (convertedData.raw_data_entry)
-        delete convertedData.raw_data_entry;
+	delete convertedData.raw_data;
+	delete convertedData.raw_data_entry;
 
-    delete convertedData.raw_data;
-
-    exit(convertedData);
+	exit(convertedData);
 };
